748027fad50966338a87ca1fad4f54e8ec8f4c40
Francisco Hauva <phauva@gmail.com>
2023-03-31 13:23:02 -0300

    Remove unused signal and effect

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,6 @@
-import { Button } from "@hope-ui/solid";
-import { Component, createEffect, createSignal } from "solid-js";
+import { Component } from "solid-js";
 
 export const Main: Component = () => {
-	const [counter, setCounter] = createSignal(0);
-
-	createEffect(() => {
-		console.log("Counter changed to", counter());
-	});
-
 	return (
 		<div class="flex h-screen items-center justify-center">
 			<p>There's nothing here :)</p>
