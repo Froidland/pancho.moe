69774759494eda914d5831271eb5dc1a70107c0a
Francisco Hauva <phauva@proton.me>
2026-03-08 04:52:17 -0300

    build: cloudflare setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	adapter: cloudflare(),
 });
